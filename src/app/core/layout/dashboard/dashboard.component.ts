67950d19da2d00880430690edf44810e10230e72
TravisMeyers <travis.meyers@hotmail.com>
2017-05-25 12:57:18 -0400

    i18n clean-up.

--- src/app/core/layout/dashboard/dashboard.component.ts
+++ src/app/core/layout/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
 
 import { LoggerService } from '../../shared/services/logger.service';
+import { environment } from './../../../../environments/environment';
 
 @Component({
   templateUrl: './dashboard.component.html',
@@ -9,10 +10,12 @@ import { LoggerService } from '../../shared/services/logger.service';
   changeDetection: ChangeDetectionStrategy.Default
 })
 export class DashboardComponent implements OnInit {
+  applicationTitle: string;
 
   constructor(private logger: LoggerService) { }
 
   ngOnInit() {
+    this.applicationTitle = environment.application.title;
     this.logger.log('[Layout.DashboardComponent] Initialized!', this);
   }
 }
