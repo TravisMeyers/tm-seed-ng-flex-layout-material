ce52887db7046c95ee177a11bbe4cfb38a3c3eab
TravisMeyers <travis.meyers@hotmail.com>
2017-05-26 14:08:55 -0400

    Card and snackbar fun!

--- src/app/core/layout/dashboard/dashboard.component.ts
+++ src/app/core/layout/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { MdSnackBar } from '@angular/material';
 
 import { LoggerService } from '../../shared/services/logger.service';
 import { environment } from './../../../../environments/environment';
@@ -12,10 +13,16 @@ import { environment } from './../../../../environments/environment';
 export class DashboardComponent implements OnInit {
   applicationTitle: string;
 
-  constructor(private logger: LoggerService) { }
+  constructor(private logger: LoggerService, public snackBar: MdSnackBar) { }
 
   ngOnInit() {
     this.applicationTitle = environment.application.title;
     this.logger.log('[Layout.DashboardComponent] Initialized!', this);
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+    });
   }
 }
