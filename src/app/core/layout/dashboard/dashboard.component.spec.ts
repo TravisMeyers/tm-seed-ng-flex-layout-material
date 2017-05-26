f16ecc298484e8b2bf6433f3c4b043a1632069a4
TravisMeyers <travis.meyers@hotmail.com>
2017-05-26 19:35:32 -0400

    Initial travis-ci implementation.

--- src/app/core/layout/dashboard/dashboard.component.spec.ts
+++ src/app/core/layout/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '@angular/material';
 
 import { SharedModule } from 'app/core/shared/shared.module';
 
@@ -14,10 +17,13 @@ describe('[Layout] DashboardComponent', () => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
+        BrowserAnimationsModule,
+        FlexLayoutModule,
+        FormsModule,
+        MaterialModule,
         SharedModule,
       ],
       declarations: [ DashboardComponent ],
-      // schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
--- src/app/core/layout/shell/shell.component.spec.ts
+++ src/app/core/layout/shell/shell.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+// import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '@angular/material';
 
 import { LayoutShellComponent } from './shell.component';
 import { SharedModule } from 'app/core/shared/shared.module';
@@ -13,10 +17,14 @@ describe('[Layout] ShellComponent', () => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
+        BrowserAnimationsModule,
+        FlexLayoutModule,
+        FormsModule,
+        MaterialModule,
         SharedModule,
       ],
       declarations: [ LayoutShellComponent ],
-      schemas: [ NO_ERRORS_SCHEMA ],
+      // schemas: [ NO_ERRORS_SCHEMA ],
     })
     .compileComponents();
   }));
