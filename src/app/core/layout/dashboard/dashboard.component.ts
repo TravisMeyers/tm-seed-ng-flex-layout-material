2720e7eb11f4942245f5342329da43b19efdbd73
TravisMeyers <travis.meyers@hotmail.com>
2017-05-26 14:56:07 -0400

    Adding grid list into the mix!

--- src/app/core/layout/dashboard/dashboard.component.ts
+++ src/app/core/layout/dashboard/dashboard.component.ts
@@ -13,6 +13,13 @@ import { environment } from './../../../../environments/environment';
 export class DashboardComponent implements OnInit {
   applicationTitle: string;
 
+  tiles = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
+  
   constructor(private logger: LoggerService, public snackBar: MdSnackBar) { }
 
   ngOnInit() {
